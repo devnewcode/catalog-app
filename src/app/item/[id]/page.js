5da9ed4045dd1f0d2abc22adc90bc909fa53c48e
devnewcode <devrathteotia2001@gmail.com>
2026-04-27 21:30:47 +0530

    pagenotfoundfix

--- src/app/item/[id]/page.js
+++ src/app/item/[id]/page.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
+export const dynamic = "force-dynamic";
+
 export async function generateStaticParams() {
   return catalog.map((item) => ({
     id: encodeURIComponent(item.itemname),
